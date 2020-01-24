918994a9932e34a92be2afe5df62ecee391e4419
AdrianxxSh <ashala3345@gmail.com>
2020-01-24 14:26:47 +0100

    last message added

--- app/src/main/java/com/example/chatapp/adapter/RoomAdapter.java
+++ app/src/main/java/com/example/chatapp/adapter/RoomAdapter.java
@@ -12,21 +12,13 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.chatapp.R;
 import com.example.chatapp.activity.ChatDetailActivity;
-import com.example.chatapp.activity.MainActivity;
-import com.example.chatapp.model.Chat;
 import com.example.chatapp.model.Room;
-import com.example.chatapp.model.User;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class RoomAdapter extends RecyclerView.Adapter<RoomAdapter.MyViewHolder> {
@@ -122,6 +114,5 @@ public class RoomAdapter extends RecyclerView.Adapter<RoomAdapter.MyViewHolder> 
 
             }
         });
-
     }
 }
