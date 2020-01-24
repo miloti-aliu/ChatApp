d7bbb4389c681d2af6f74331debd3088f41256ab
AdrianxxSh <ashala3345@gmail.com>
2020-01-24 14:12:46 +0100

    Merge branch 'master' of C:\Users\solaborate\AndroidStudioProjects\ChatApp with conflicts.

--- app/src/main/java/com/example/chatapp/adapter/RoomAdapter.java
+++ app/src/main/java/com/example/chatapp/adapter/RoomAdapter.java
@@ -7,43 +7,61 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.chatapp.R;
 import com.example.chatapp.activity.ChatDetailActivity;
 import com.example.chatapp.activity.MainActivity;
+import com.example.chatapp.model.Chat;
 import com.example.chatapp.model.Room;
-
-import org.w3c.dom.Text;
+import com.example.chatapp.model.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class RoomAdapter extends RecyclerView.Adapter<RoomAdapter.MyViewHolder> {
 
-    private List<Room> rooms = new ArrayList<>();
-    private String lastMessage;
+    private List<Room> rooms;
 
-    public RoomAdapter(){
 
+    String theLastMessage;
+
+    public RoomAdapter(List<Room> rooms) {
+        this.rooms = rooms;
     }
 
     @NonNull
     @Override
     public RoomAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout
-                .item_room,parent,false);
+                .item_room, parent, false);
         return new MyViewHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull RoomAdapter.MyViewHolder holder, int position) {
-        holder.fillView(rooms.get(position));
-    }
 
-    public void addRoom(Room room){
-        rooms.add(room);
-        notifyItemInserted(getItemCount());
+        final Room room = rooms.get(position);
+
+        holder.roomname.setText(rooms.get(position).getRoomname());
+
+        lastMessage(room.getId(), holder.last_Msg);
+
+        holder.itemView.setOnClickListener(v -> {
+            Intent intent = new Intent(v.getContext(), ChatDetailActivity.class);
+            intent.putExtra("roomId", rooms.get(position).getId());
+            v.getContext().startActivity(intent);
+        });
     }
 
     @Override
@@ -53,23 +71,57 @@ public class RoomAdapter extends RecyclerView.Adapter<RoomAdapter.MyViewHolder> 
 
     class MyViewHolder extends RecyclerView.ViewHolder {
         TextView roomname;
-        TextView lastMsg;
+        TextView last_Msg;
+
         MyViewHolder(@NonNull View itemView) {
             super(itemView);
 
             roomname = itemView.findViewById(R.id.room_name);
-            lastMsg = itemView.findViewById(R.id.last_msg);
+            last_Msg = itemView.findViewById(R.id.last_msg);
         }
+    }
 
-        void fillView(Room room) {
-            roomname.setText(room.getRoomname());
-            lastMsg.setText(room.getLastMsg());
-            itemView.setOnClickListener(v -> {
-                Intent intent = new Intent(v.getContext(), ChatDetailActivity.class);
-                intent.putExtra("roomId",room.getId());
-                intent.putExtra("roomName",room.getRoomname());
-                v.getContext().startActivity(intent);
-            });
-        }
+    private void lastMessage(String roomid, TextView last_msg) {
+
+        theLastMessage = "default";
+        Query query = FirebaseDatabase.getInstance().getReference().child("rooms").child(roomid).child("chats").orderByKey().limitToLast(1);
+
+        query.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                theLastMessage = dataSnapshot.child("message").getValue().toString();
+                switch (theLastMessage) {
+                    case "default":
+                        last_msg.setText("No Message");
+                        break;
+
+                    default:
+                        last_msg.setText(theLastMessage);
+                        break;
+                }
+                theLastMessage = "default";
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
     }
 }
